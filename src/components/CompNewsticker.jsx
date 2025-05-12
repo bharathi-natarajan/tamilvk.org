3a1aeea94ba1620527ed1877e3cc535b9a49b3ca
bharathi natarajan <bharathi_n@msn.com>
2025-05-12 12:56:24 +0530

    Correct newticker error

--- src/components/CompNewsticker.jsx
+++ src/components/CompNewsticker.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import './CompNewsTicker.css'
+import './CompNewsticker.css'
 import { newsTicker } from '../data/newsData'
 
 const CompNewsTicker = () => {
